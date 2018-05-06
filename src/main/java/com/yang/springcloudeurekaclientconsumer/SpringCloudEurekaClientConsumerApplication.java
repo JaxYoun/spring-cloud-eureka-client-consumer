5866a31e8ba507e1e7e77608d13fb8f5e9cca045
JaxYoun <gao88jie@qq.com>
2018-05-06 22:29:01 +0800

    eureka消费者初始提交

---
+++ src/main/java/com/yang/springcloudeurekaclientconsumer/SpringCloudEurekaClientConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.yang.springcloudeurekaclientconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaClientConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaClientConsumerApplication.class, args);
+	}
+}
